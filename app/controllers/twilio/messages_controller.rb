e1b61f5a5168886db2768b5ef53cb2f092926830
Michelle Brien <michelle.brien12@gmail.com>
2018-10-15 22:34:27 +0100

    fixed typo

--- app/controllers/twilio/messages_controller.rb
+++ app/controllers/twilio/messages_controller.rb
@@ -5,7 +5,7 @@ class Twilio::MessagesController < ApplicationController
     body = params['Body']
     spotify = Spotify.new(ENV['SPOTIFY_USER_ID'], ENV['SPOTIFY_TOKEN'], ENV['SPOTIFY_REFRESH_TOKEN']) 
 
-    track = spotify.strack_search(body)
+    track = spotify.track_search(body)
     message = "Did you want to add _#{track.name}_ by _#{track.artists.map(&:name).to_sentence}_?"
     response = Twilio::TwiML::MessagingResponse.new
     response.message(body: message)
