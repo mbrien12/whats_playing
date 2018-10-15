879faadaca1dec4df096bd5ed2ccfdcb3e82ea69
Michelle Brien <michelle.brien12@gmail.com>
2018-10-15 22:50:26 +0100

    fixed typo

--- app/controllers/twilio/messages_controller.rb
+++ app/controllers/twilio/messages_controller.rb
@@ -1,15 +1,14 @@
 class Twilio::MessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
-
   def create
     body = params['Body']
-    spotify = Spotify.new(ENV['SPOTIFY_USER_ID'], ENV['SPOTIFY_TOKEN'], ENV['SPOTIFY_REFRESH_TOKEN']) 
+    spotify = Spotify.new(ENV['SPOTIFY_USER_ID'], ENV['SPOTIFY_TOKEN'], ENV['SPOTIFY_REFRESH_TOKEN'])
 
     track = spotify.track_search(body)
     message = "Did you want to add _#{track.name}_ by _#{track.artists.map(&:name).to_sentence}_?"
+
     response = Twilio::TwiML::MessagingResponse.new
     response.message(body: message)
     render xml: response.to_xml
   end
-
 end
