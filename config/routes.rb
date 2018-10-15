cb7e94d55f7cf9b679140b9b34b802087575116e
Michelle Brien <michelle.brien12@gmail.com>
2018-10-15 22:30:26 +0100

    added twilio and spotify integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :twilio do
+    post 'messages', to: 'messages#create'
+  end
+
   root 'home#index'
   get '/auth/spotify/callback', to: 'home#callback'
 end
